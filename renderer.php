4c7dc6a81f1860abf4140da5bb09aaa89984442f
Anay Infowind <anay.infowind@gmail.com>
2020-10-28 15:28:33 +0530

    Added theme setting

--- renderer.php
+++ renderer.php
@@ -135,10 +135,16 @@ class block_tb_up_courses_renderer extends plugin_renderer_base {
             'u.lang, u.timezone, u.lastaccess, u.mnethostid, u.imagealt, r.name AS rolename, r.sortorder, ' .
             'r.shortname AS roleshortname, rn.name AS rolecoursealias';
 
-        if($config->upcoming_showasslider == 1){    
-            $html .= html_writer::start_div('tb_up_courses_list owl-carousel owl-theme');
+        if($config->upcoming_style == 0){
+            $colorstyle = 'style_light';
         }else{
-            $html .= html_writer::start_div('tb_up_courses_list');
+            $colorstyle = 'style_dark';
+        }
+
+        if($config->upcoming_showasslider == 1){    
+            $html .= html_writer::start_div('tb_up_courses_list owl-carousel owl-theme '.$colorstyle);
+        }else{
+            $html .= html_writer::start_div('tb_up_courses_list '.$colorstyle);
         }  
         foreach ($courses as $key => $course) {
             $percent = block_tb_up_courses_progress_percent($course);
