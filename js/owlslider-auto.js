dba8fa84c67f4a8810f43320a552765452497075
Anay Infowind <anay.infowind@gmail.com>
2021-04-21 16:09:06 +0530

    Item numbers

--- js/owlslider-auto.js
+++ js/owlslider-auto.js
@@ -2,11 +2,42 @@ require(["jquery"],function($) {
     
     $(document).ready(function() {
 
-        $('.tb_up_courses_list').owlCarousel({
+        $('.layout_boxed .tb_up_courses_list').owlCarousel({
             loop: true,
             margin: 10,
             responsiveClass: true,
             autoplay: true,
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: true
+                },
+                600: {
+                    items: 2,
+                    nav: false
+                },
+                1000: {
+                    items: 2,
+                    nav: true,
+                    dots: false,
+                    margin: 20
+                },
+                1300: {
+                    items: 3,
+                    nav: true,
+                    dots: false,
+                    margin: 20
+                }
+            }
+        });
+
+        $('body:not(.layout_boxed) .tb_up_courses_list').owlCarousel({
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            autoplay: true,
+            dots: false,
             responsive: {
                 0: {
                     items: 1,
@@ -30,5 +61,6 @@ require(["jquery"],function($) {
                 }
             }
         });
+
     });    
 });
